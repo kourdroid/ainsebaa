8e208338097f3b885be8f0dc016e7090c295df2b
Kourdroid <kourdroid@gmail.com>
2023-09-29 17:21:22 +0100

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+// burger menubar
+
+document.addEventListener("DOMContentLoaded", function () {
+  const burgerIcon = document.querySelector(".fa-bars");
+  const menuItems = document.querySelector(".font-normal");
+
+  burgerIcon.addEventListener("click", function () {
+    menuItems.classList.toggle("hidden");
+  });
+});
+
+
+window.addEventListener('scroll',()=>{
+  let header = document.querySelector('header')
+  header.classList.toggle("sticky",window.scrollY > 0);
+
+})
+
+
+
